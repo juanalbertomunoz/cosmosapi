0f69cf29d4e6a32686dbb5411690ab09c180f1c5
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-08 10:04:33 -0500

    Fix order connect mongodb

--- config/mongo.js
+++ config/mongo.js
@@ -1,25 +1,18 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 const uri = "mongodb+srv://" + process.env.USER + ":"+ process.env.PASSWORD + "@cluster0.qrr7cbm.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoose = require ("mongoose")
-require('dotenv').config()
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("**** DB CONNECTED ****");
+  } catch (error) {
+    console.log("**** ERROR DB CONNECTION ****", error);
+  }
+};
 
-const dbConnect = () => {
-    mongoose.set('strictQuery', false);
-    mongoose.connect(uri,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true, 
-                
-        }, 
-        (err, res) => {
-            if(!err){
-                console.log('**** DB CONNECTED ****')
-            }else{
-                console.log('**** ERROR DB CONNECTION ****')
-            }
-    
-        }
-    )
-}
-
-module.exports = dbConnect
+module.exports = dbConnect;
