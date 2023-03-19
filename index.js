77360d091e34ec1b9c5eafcd4674e908279f4e29
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-19 13:35:37 -0500

    Fix socket 1.2

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ io.on("connection", (socket) => {
 */
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 /**
  * SWAGGER
