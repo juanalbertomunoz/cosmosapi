f92a03cf78d0056a422f762a327689558251ebc1
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-19 15:02:47 -0500

    Fix socket 1.4

--- controllers/alert.js
+++ controllers/alert.js
@@ -30,53 +30,41 @@ const newAlert = async (req, res) => {
     io.emit('alertsequrete', data);
     res.send({data});
   } catch (e) {
+    console.error(e);
     handleHttpError(res, "ERROR_CREATE_CREATE_ALERT...");
   }
 };
 
-/**
- * GET ALERT BY KEY
- * @param {*} req 
- * @param {*} res 
- */
 const getAlert = async (req, res) => {
   try{
-
-    //req = matchedData(req)
-    //const {id} = req.params.id
-    const id = req.params.id
-    //console.log(id)
+    const id = req.params.id;
     const data = await Alert.find({
       key :  id,
       estado: false
-    })
+    }).catch((e) => {
+      console.error(e);
+      handleHttpError(res, "ERROR_GET_ALERT_HOME");
+    });
     console.log (data)
     res.send(data)
   }catch(e){
-    handleHttpError(res, "ERROR_GET_ALERT_HOME")
+    console.error(e);
+    handleHttpError(res, "ERROR_GET_ALERT_HOME");
   }
 }
 
-/**
- * Update Alert
- * @param {*} req 
- * @param {*} res 
- */
 const updateAlert = async (req, res) => {
   try {
-    //const body = matchedData(req)
-    //const body = req.body
-    //console.log("body", body)
-    //const bodyclean = req.body
-    const data = await Alert.findByIdAndUpdate(req.params.id, req.body)
-    //console.log("data", data)
+    const data = await Alert.findByIdAndUpdate(req.params.id, req.body).catch((e) => {
+      console.error(e);
+      handleHttpError(res, "ERROR_UPDATE_ALARM");
+    });
     res.send({ data })
-    } catch (e) {
-      //console.log(e)
-      handleHttpError(res, "ERROR_UPDATE_ALARM")
-    }
-  
-}
+  } catch (e) {
+    console.error(e);
+    handleHttpError(res, "ERROR_UPDATE_ALARM");
+  }
+};
 
 
 module.exports = {newAlert, getAlert, updateAlert}
