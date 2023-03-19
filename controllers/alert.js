f7a47287b1ef2b53bbdd445328abc6cc479bdd89
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-19 15:32:35 -0500

    Fix socket 1.5

--- controllers/alert.js
+++ controllers/alert.js
@@ -27,13 +27,20 @@ io.on("connection", (socket) => {
 const newAlert = async (req, res) => {
   try {
     const data = await Alert.create({key: req.params.id, msm: req.params.id2, mac: req.params.id3});
-    io.emit('alertsequrete', data);
-    res.send({data});
+    io.emit('alertsequrete', data, (error) => {
+      if (error) {
+        console.error(error);
+        handleHttpError(res, "ERROR_EMITTING_ALERT...");
+      } else {
+        res.send({data});
+      }
+    });
   } catch (e) {
     console.error(e);
     handleHttpError(res, "ERROR_CREATE_CREATE_ALERT...");
   }
 };
+
 
 const getAlert = async (req, res) => {
   try{
