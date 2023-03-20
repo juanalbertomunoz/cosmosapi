83cf7e96b8f712555c22eb0a8ad54a14fe8be3ef
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-20 13:44:35 -0500

    Fix socket 1.24 cors

--- config/socket.js
+++ config/socket.js
@@ -1,11 +1,13 @@
 /*
-const {server} = require('../index');
-const io = require('socket.io')(server, {
-    cors: {
-      origin: ['https://flourishing-valkyrie-7a4fe1.netlify.app/', '*'],
-      methods: ['GET', 'POST']
-    }
-  });
+const server = require ('../index')
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "https://flourishing-valkyrie-7a4fe1.netlify.app/",
+    methods: ["GET"],
+  },
+});
+
   
 
 module.exports = io;
--- index.js
+++ index.js
@@ -1,64 +1,74 @@
-require("dotenv").config();
+require('dotenv').config();
 //const { ioMonitor } = require('socket.io-monitor');
 //const socketIO = require('socket.io');
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require("cors")
+const dbConnect = require("./config/mongo")
+const { Server } = require("socket.io");
 const app = express();
-const dbConnect = require("./config/mongo");
+app.use(cors())
+app.use(express.json())
+const PORT = process.env.PORT || 3001;
 //const newAlert = require('./controllers/alert').newAlert
-
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-/**
- * SWAGGER
- */
-require("./swagger")(app);
-
 const http = require("http");
 const server = http.createServer(app);
-
-const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "https://flourishing-valkyrie-7a4fe1.netlify.app/",
     methods: ["GET"],
   },
 });
+//const io = require('./config/socket')
 
-const authRoutes = require("./routes/auth");
+
+
+/*
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+*/
+/**
+ * SWAGGER
+ */
+require('./swagger')(app);
+
+/**
+ * ROUTES
+ */
+const authRoutes = require("./routes/auth")
 //const regShellyRoutes = require("./routes/alert")
-const homeRoutes = require("./routes/home");
-// const alertRoutes = require("./routes/alert")
+const homeRoutes = require("./routes/home")
+const alertRoutes = require("./routes/alert")
+
+
 
 //Middleware
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes)
 //app.use("/api/regShelly", regShellyRoutes)
-// app.use("/api/alerts", alertRoutes)
-app.use("/api/homes", homeRoutes);
+app.use("/api/alerts", alertRoutes)
+app.use("/api/homes", homeRoutes)
 //app.use('/api/alertsecurity', socketRoutes(io));
+
+dbConnect()
+// Configurar socket.io-monitor
+//ioMonitor(io, { port: 8082, path: '/socket.io-monitor' });
+// Exponer la interfaz de usuario web en la ruta /socket.io/monitor
+io.on("connection", (socket) => {
+  console.log("A client connected to the socket.io server");
+});
 
 io.on("connection", (socket) => {
   console.log("New Connection Socket");
   socket.emit("alertsequrete");
 });
-
-app.set("port", 3001);
-
 app.get("/", (req, res) => {
   res.send("<span>Cosmos Server</span>");
 });
 
-dbConnect();
-// Configurar socket.io-monitor
-//ioMonitor(io, { port: 8082, path: '/socket.io-monitor' });
-// Exponer la interfaz de usuario web en la ruta /socket.io/monitor
-
-server.listen(app.get("port"), () => {
-  console.log(`Server listen in the port: ${app.get("port")}`);
+server.listen(PORT, () => {
+    console.log('Server express is connected in ' + PORT + ' PORT')
 });
+
+//module.exports = {server};
