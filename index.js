34778562bdb916fc88a5bcf5bff86dd91fd4b9cf
Annunnaki <juanalbertomunozsalinas@gmail.com>
2023-03-20 10:43:14 -0500

    Fix socket 1.14

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json())
+/*
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
+*/
 /**
  * SWAGGER
  */
